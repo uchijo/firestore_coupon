54e1845cc95315c9058612937c63eb2beb10c47c
Uchijo <kgtnttki.yuta@gmail.com>
2022-07-03 17:58:06 +0900

    anonymous login enabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firestore_coupon/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await attemptLogin();
   runApp(
     const ProviderScope(
       child: MyApp(),
@@ -37,5 +45,26 @@ class MyHomePage extends HookConsumerWidget {
         child: Text('test'),
       ),
     );
+  }
+}
+
+Future<void> attemptLogin() async {
+  final currentUser = FirebaseAuth.instance.currentUser;
+  if (currentUser != null) {
+    debugPrint(' === already logged in. id: ${currentUser.uid}');
+    return;
+  }
+
+  try {
+    final userCredential = await FirebaseAuth.instance.signInAnonymously();
+    debugPrint(' === logged in.');
+  } on FirebaseAuthException catch (e) {
+    switch (e.code) {
+      case "operation-not-allowed":
+        debugPrint('anonymous auth hasn\'t been enabled for this project.');
+        break;
+      default:
+        debugPrint('unknown error. e: $e');
+    }
   }
 }
