5460171b4f18347533b208e88b4b069cdf813098
Uchijo <kgtnttki.yuta@gmail.com>
2022-07-03 17:10:02 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/firestore_coupon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firestore_coupon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends HookConsumerWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('coupon test'),
+      ),
+      body: const Center(
+        child: Text('test'),
+      ),
+    );
+  }
+}
